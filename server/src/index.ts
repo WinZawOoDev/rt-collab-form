4a15e163de86184257afd2febef9f6cae3bbdd37
WinZawOo <winzawoo.dev@gmail.com>
2026-02-22 23:48:44 +0630

    chore: basic auth and database(prisma orm) testign

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
 
 const app = new Hono()
+
+app.use('/api/*', basicAuth({
+  username: 'admin',
+  password: 'password'
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/api/data', (c) => {
+  return c.json({ message: 'This is protected data.' })
+});
 
 export default app
